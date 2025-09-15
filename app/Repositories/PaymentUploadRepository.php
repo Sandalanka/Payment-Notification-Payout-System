c5de588f201ef2f58dc6fa8ace7608c97c1aaa10
Sandalanka <1415sandalanka@gmail.com>
2025-09-15 17:52:46 +0530

    payment excel upload queue validation

--- app/Repositories/PaymentUploadRepository.php
+++ app/Repositories/PaymentUploadRepository.php
@@ -5,6 +5,10 @@ use App\Interfaces\PaymentUploadRepositoryInterface;
 use App\Jobs\ProcessPaymentFileJob;
 use App\Classes\ApiCatchErrors;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\PaymentImport;
 
 class PaymentUploadRepository implements  PaymentUploadRepositoryInterface
 {
@@ -17,11 +21,23 @@ class PaymentUploadRepository implements  PaymentUploadRepositoryInterface
     public function uploadExcel(UploadedFile $file)
     {
         try{
-            $path = $file->store('payment_uploads');
-            ProcessPaymentFileJob::dispatch($path);
+             Excel::queueImport(new PaymentImport, $file);
+          
         }catch(\Exception $exception){
             return ApiCatchErrors::throw($exception);
          }
+    }
+
+    public function uploadS3(UploadedFile $file, string $directory = 'payment-files'){
+        try{
+            $fileName = $directory.'/'.date('Y/m/d').'/'.Str::uuid().'.'.$file->getClientOriginalExtension();
+            Storage::disk('s3')->put($fileName, file_get_contents($file->getRealPath()));
+            return $fileName;
+
+        }catch(\Exception $exception){
+            return ApiCatchErrors::throw($exception);
+         }
+        
     }
 
 }
