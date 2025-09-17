fd40cf92afa9eb2e159d86a9cb250c87ef7f4ca5
Sandalanka <1415sandalanka@gmail.com>
2025-09-17 18:57:54 +0530

    change console withh everyMinute

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('payouts:send')->dailyAt('00:10')->timezone('Asia/Colombo');
+//Schedule::command('payouts:send')->dailyAt('00:10')->timezone('Asia/Colombo');
+Schedule::command('payouts:send')->everyMinute();
