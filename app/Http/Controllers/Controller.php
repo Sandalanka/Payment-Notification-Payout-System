9c66a1c8564729df523f8140f6970c2a4dca8447
Sandalanka <35499094+Sandalanka@users.noreply.github.com>
2025-11-18 21:44:25 +0530

    feat: differ commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,37 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    /**
+     * Summary: send success response
+     * 
+     * @param  mixed $result
+     * @param  mixed $message
+     * @return void
+     */
+    public function sendResponse(array $result = [] , string $message=, $code=200){
+        $response=[
+            'success' => true,
+            'data'    => $result
+        ];
+        if(!empty($message)){
+            $response['message'] =$message;
+        }
+        return response()->json($response, $code);
+    }
+    
+    /**
+     * Summary: send error response
+     * 
+     * @param  mixed $error
+     * @return void
+     */
+    public function sendError($error, $code=400)
+    {
+    	$response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        return response()->json($response, $code);
+    }
 }
